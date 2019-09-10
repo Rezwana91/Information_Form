3746396f53cc14a23c3d91373d98e288ea1b6a2f
Rezwana91 <55046929+Rezwana91@users.noreply.github.com>
2019-09-10 06:51:21 +0600

    Add files via upload

---
+++ Information_Form/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Information_Form
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "";
+            String age  = "";
+            String address = "";
+            String contact_no = "";
+
+            Console.WriteLine("Please Write your Name ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Please Write your Age ");
+            age = Console.ReadLine();
+
+            Console.WriteLine("Please Write your Address ");
+            address = Console.ReadLine();
+
+            Console.WriteLine("Please Write your Contact_no ");
+            contact_no = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Name       :" + name +".");
+            Console.WriteLine("Age        :" + age );
+            Console.WriteLine("Address    :" + address +".");
+            Console.WriteLine("Contact_no :" + contact_no);
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
